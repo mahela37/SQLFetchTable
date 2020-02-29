3f32b03cc763c34bc9c368fbfbf329d59605fd09
mahela37 <35088078+mahela37@users.noreply.github.com>
2020-02-29 11:42:29 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,87 @@
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os,time
+app = Flask(__name__)
+
+listStartIndex=0
+listEndIndex=3
+returnData=[]
+returnDataLength=0
+
+#def for returning dummy sql data
+def getSqlData():
+    f=open("recipe.txt", "r")
+    fileContents=f.readlines()
+    f.close()
+    returnData=[]
+    for line in fileContents:
+        line=line.split(",")
+        temp=[]
+        temp.append(int(line[0]))
+        temp.append(line[1].strip())
+        temp.append(line[2].strip())
+        returnData.append(temp)
+    for item in returnData:
+        #print(item)
+        ""
+    global returnDataLength
+    returnDataLength=len(returnData)
+
+    return returnData
+
+#if not session.get('logged_in'):   useful snippet.
+
+@app.route('/login.html')
+def login_page():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+        global returnData
+        returnData = getSqlData()
+        trimmedData = returnData[listStartIndex:listEndIndex]
+        return render_template('table.html', dataArray=trimmedData)
+    else:
+        return render_template('login.html',wrongLogin=1)
+
+@app.route('/scrollPage',methods=['POST'])
+def scrollPage():
+    global listStartIndex
+    global listEndIndex
+    global returnDataLength
+    if((request.form['direction']=='forward')and(listEndIndex<returnDataLength)):
+        listStartIndex = listStartIndex + 3
+        listEndIndex = listEndIndex + 3
+
+    if ((request.form['direction'] == 'backward')and(listStartIndex!=0)):
+        listStartIndex = listStartIndex - 3
+        listEndIndex = listEndIndex - 3
+
+    #returnData = getSqlData()
+    global returnData
+    trimmedData = returnData[listStartIndex:listEndIndex]
+    return render_template('table.html', dataArray=trimmedData)
+
+@app.route('/refreshPage')
+def refreshPage():
+    if not session.get('logged_in'):
+        return "NOT LOGGED IN"
+
+    global returnData
+    returnData = getSqlData()
+    trimmedData = returnData[listStartIndex:listEndIndex]
+    return render_template('table.html', dataArray=trimmedData)
+
+@app.route('/postRefreshPage',methods=['POST'])
+def postRefreshPage():
+    global returnData
+    returnData = getSqlData()
+    trimmedData = returnData[listStartIndex:listEndIndex]
+    return render_template('table.html', dataArray=trimmedData)
+
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+    app.run(debug=True,host='0.0.0.0', port=4000)
+    #getSqlData()
